09d7f7e4dc9600c19e39fa17c1eec0c0c8a29610
Petr Dlouhý <petr.dlouhy@email.cz>
2014-01-03 11:28:11 +0100

    add linsadd linsk overpass api

--- trasy.sh
+++ trasy.sh
@@ -3,17 +3,20 @@
 #source="http://osm.bearstech.com/osmxapi/api/0.6/"
 
 #source="http://jxapi.openstreetmap.org/xapi/api/0.6/"
-source="http://open.mapquestapi.com/xapi/api/0.6/"
+#source="http://open.mapquestapi.com/xapi/api/0.6/"
+source="http://www.overpass-api.de/api/xapi?"
 
 # --------------------
 
 # JXAPI
 wget "${source}relation[operator=cz:KČT|cz:KCT|cz:kčt|cz:kct|CZ:KCT|CZ:KČT]" -O kct.osm
+#wget 'http://overpass-api.de/api/interpreter?data=(relation["operator"="cz:kčt"];relation["operator"="cz:kct"];relation["operator"="cz:KČT"];relation["operator"="CZ:KCT"];relation["operator"="cz:KCT"];relation["operator"="CZ:KČT"];);out meta;way(r);out meta;node(w);out meta;' -O kct.osm
 
 # XAPI
 #wget "${source}relation[operator=cz:KČT|operator=cz:KCT|operator=cz:kčt|operator=cz:kct|operator=CZ:KCT|operator=CZ:KČT]" -O kct.osm
 #wget "${source}relation[kct_red=*][kct_green=*][kct_blue=*][kct_yellow=*][kct_white=*]" -O kct.osm
 
+#wget 'http://overpass-api.de/api/interpreter?data=(way["kct_red"];way["kct_blue"];way["kct_yellow"];way["kct_green"];way["kct_white"];way["kct_learning"]);out meta;node(w);out meta;' -O way.osm
 #wget "${source}way[kct_green|kct_red|kct_blue|kct_yellow|kct_white|kct_none=*]" -O way.osm
 #wget "${source}way[kct_green=*][kct_red=*][kct_blue=*][kct_yellow=*][kct_white=*][kct_none=*]" -O way.osm
 
